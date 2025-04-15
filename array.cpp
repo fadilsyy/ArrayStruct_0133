489a7501553f8291b382e94e62140d20fcddc18a
fadilsyy <fadilasytar@gmail.com>
2025-04-15 21:20:10 +0800

    array

--- array.cpp
+++ array.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 int nilai[]={1,2,3};
-//deklarasi array dengan ukuran, diisi data awal
 int skor[4]={12,24,48,64};
 //deklarasi array dengan ukuran, tanpa nilai awal
 string nama[3];
